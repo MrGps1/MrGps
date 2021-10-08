114379fb32fe7f16e6bb37ab21914266309c7807
MrGps1 <87803991+MrGps1@users.noreply.github.com>
2021-10-08 14:34:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,20 @@ import setuptools
 with open("README.md", "r", encoding="utf-8")
     long_description = fh.read()
 
+with open("requirements.txt",'r') as fr:
+    requires = fr.read().split('\n')
+    
 setuptools.setup(
     name="MrGps",
     version="0.0.1",
     author="MrGps1",
     author_email="author@example.com",
-    description="A small example package",
+    description="Short EveryThing In Seconds",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MrGps",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/MrGps1/MrGps/",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -23,4 +26,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=requires,
 )
